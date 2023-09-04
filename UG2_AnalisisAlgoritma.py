cea52ac2344038dafaf167dca3ec78e3aed8ee8c
Kezia Trifena <93313412+keziatrifena@users.noreply.github.com>
2023-09-04 14:11:10 +0700

    Add files via upload

---
+++ UG2_AnalisisAlgoritma.py
@@ -0,0 +1,11 @@
+def magical_series_iteratif(n):
+    pass
+    #isi codingan Anda disini
+
+def magical_series_recursive():
+    pass
+    #isi codingan Anda disini
+
+if __name__ == '_main_':
+    pass
+    #isi codingan Anda disini
